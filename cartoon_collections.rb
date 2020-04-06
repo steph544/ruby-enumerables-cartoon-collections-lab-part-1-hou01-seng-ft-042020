a409bc7ecf592bdae8c93300db1b51e9598a7926
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-06 19:40:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,8 @@ end
 end
 
 def list_dwarves(array)
+  array.each_with_index do |element, index|
+    puts (index+1). + element 
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
